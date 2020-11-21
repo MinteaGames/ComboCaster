909b69081b552d90714854e59e4996ae7222fece
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2020-11-21 14:03:36 +0000

    Create player sprite manager script
    Created a player animation controller script, and wrote code to allow the player to spin in 8 directions

---
+++ ComboCaster/Assets/Scripts/Player/playerSpriteManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSpriteManager : MonoBehaviour
+{
+
+    public int rotateState;
+    Transform rotateDir;
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateDir = transform.GetChild(0).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateState = Convert.ToInt32(rotateDir.eulerAngles.z / 45.0f);
+
+        anim.SetInteger("direction", rotateState);
+    }
+}
